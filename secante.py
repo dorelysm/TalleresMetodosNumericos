3868a2aba7824e630453d16317b6ae1262f0429c
dorelysm <martinezdorelys@gmail.com>
2022-09-06 20:02:57 -0500

    correcion metodo secante

---
+++ secante.py
@@ -0,0 +1,36 @@
+"""
+Programa para hallar una raíz por el 
+método de la secante
+"""
+
+import math
+
+#Funciones
+"""
+def f(x):
+    x = math.radians(x)
+    resultado = -math.cos(x) + x
+    return resultado
+"""
+
+def secante(f, x1, x2, tol):
+    error = 1e3
+    n = 0
+    x3 = 0
+    while error > tol:
+        x3 = x1-((x2-x1)/(f(x2)-f(x1)))*f(x1)
+        x1 = x2
+        x2 = x3
+        error = abs(f(x3))
+        n *= 1
+    return x3, n
+
+Xsubcero = int(input('Ingrese x0: '))
+Xsubuno = int(input('Ingrese x1: '))
+tol = 1e-4
+f=lambda x:x-math.cos(x)
+
+resultado, n = secante(f, Xsubcero, Xsubuno, tol)
+
+print("Raíz: {:4f}".format(resultado))
+print("Número de iteraciones: {:d}".format(n))
