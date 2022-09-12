a93aa1f1d8bb84d5e00b93800cf5a4b0faf38d4f
Forisush <jeidera-forondap@unilibre.edu.co>
2022-09-12 11:02:12 -0500

    modificacion

--- metodo_secante.py
+++ metodo_secante.py
@@ -5,28 +5,34 @@ método de la secante
 
 import math
 
-#Funciones
+# Funciones
+
+
 def funcion(x):
     x = math.radians(x)
     resultado = math.cos(x) + x
     return resultado
 
+
 def secante(funcion, Xsubcero, Xsubuno, tol):
     error = 1e3
     n = 0
     Xsubn = 0
-    
+
     while error > tol:
-        Xsubn = Xsubcero - ((Xsubuno - Xsubcero)/(funcion(Xsubuno) - funcion(Xsubcero))) / funcion(Xsubcero)
+        Xsubn = Xsubcero - \
+            ((Xsubuno - Xsubcero)/(funcion(Xsubuno) -
+             funcion(Xsubcero))) / funcion(Xsubcero)
         Xsubcero = Xsubuno
         Xsubuno = Xsubn
-        
+
         error = abs(funcion(Xsubn))
         n += 1
-                
+
     return Xsubn, n
-    
-#Entradas
+
+
+# Entradas
 #func = input('Ingrese f(x): ')
 Xsubcero = int(input('Ingrese x0: '))
 Xsubuno = int(input('Ingrese x1: '))
