cab99db0902e68b4242a64bd8d1003271f78dd44
JuanB12 <jcamilobb03@gmail.com>
2022-09-15 11:32:55 -0500

    Biseccion terminado

--- biseccion.py
+++ biseccion.py
@@ -1,28 +1,38 @@
-from pickle import NONE
-from re import A
-import numpy as np
 import math
-#import matplotlib.pyplot as plt
 
-fx = (lambda x: math.pow(x, 3) + 4 * math.pow(x, 2-10))
-a = 1
-b = 2
-tolera = 0.0001
-c = (a + b) / 2
-fa = fx(a)
-fb = fx(b)
-fc = fx(c)
+iteraciones = 0
+f_max = 999999
 
-valor = np.sign(fa) * np.sign(fb)
+e = input("Ingrese la función a resolver: ")
+x_a = float(input("Ingrese el valor de la cota superior: "))
+x_b = float(input("Ingrese el valor de la cota inferior: "))
+tolera = float(input("Ingrese el valor de la tolerancia: "))
 
-if valor < 0:
-    a = a 
-    b = c
-elif valor > 0:
-    a = c
-    b = b
 
-tramo = b - a
-print(c)
-#a = float(input(print("Digite el valor de (A): ")))
-#b = float(input(print("Digite el valor de (B): ")))
+def biseccion(x):
+    return eval(e)
+
+
+while abs(f_max) >= tolera:
+    puntoIntermedio = ((x_a + x_b) / 2)
+    f_a = biseccion(x_a)
+    f_b = biseccion(x_b)
+    f_c = biseccion(puntoIntermedio)
+
+    iteraciones += 1
+
+    print(" x_a: " ,x_a, " x_b: ", x_b, " c: ", puntoIntermedio, " f_c: ", f_c, " N_itera: ", iteraciones)
+    #print(f"El valor ingresado de x_a es el siguiente {x_a}")
+    #print(f"El valor ingresado de x_a es el siguiente {x_b}")
+    #print(f"Punto medio es {f_c}")
+    #print(f"El total de iteraciones que hubo fue el siguiente: {iteraciones}")
+
+    if ((f_a * f_c) < 0):
+        x_b = puntoIntermedio
+    elif ((f_b * f_c) < 0):
+        x_a = puntoIntermedio
+
+    if abs(f_c) < tolera:
+        break
+
+print(f"La raíz buscada es {puntoIntermedio}")
