4a80748521d56e00c4d8953e93d5a4cfbde62a53
dorelysm <martinezdorelys@gmail.com>
2022-11-29 01:50:51 -0500

    gauss-jordan

--- Tercer_Corte/gaussJordan.py
+++ Tercer_Corte/gaussJordan.py
@@ -17,10 +17,28 @@ def gauss_jordan():
     n = int(input("Ingrese el tamaño de la matriz: "))
     matrizA = crear_matriz(n, n+1)
     solucion = np.zeros(n)
-    print(solucion)
     
+    for i in range(n):
+        if matrizA[i][i] == 0:
+            print('No es posible continuar, division por cero')
+            
+        for j in range(i+1, n):
+            ratio = matrizA[j][i]/matrizA[i][i]
+            
+            for k in range(n+1):
+                matrizA[j][k] = matrizA[j][k] - ratio * matrizA[i][k]
+                
+    solucion[n-1] = matrizA[n-1][n]/matrizA[n-1][n-1]
     
-    return matrizA
+    for i in range(n-2, -1, -1):
+        solucion[i] = matrizA[i][n]
+        
+        for j in range(i+1, n):
+            solucion[i] = solucion[i] - matrizA[i][j] * solucion[j]
+            
+        solucion[i] = solucion[i] / matrizA[i][i]
+    
+    return solucion
 
-matrizA=gauss_jordan()        
-print(matrizA)
+solucion=gauss_jordan()        
+print(solucion)
